c070a9e908c83365c75dc0499d520bbc2ea898b5
theokrueger <me@theokrueger.dev>
2026-04-05 19:58:47 -0400

    pipeline shtuff

--- parakeyt_pipeline.py
+++ parakeyt_pipeline.py
@@ -50,9 +50,9 @@ check_or_clone_repo("https://github.com/parakeyt/parakeyt-case", "case")
 check_or_clone_repo("https://github.com/parakeyt/parakeyt-firmware", "firmware")
 
 # read config
-print(f"Reading Config {args.input_file}")
-with open("./config.json", "r") as f:
-    config = json.load(f)
+#print(f"Reading Config {args.input_file}")
+#with open("./config.json", "r") as f:
+#    config = json.load(f)
 
 # generate pcb
 os.chdir(SCRIPT_DIR + 'pcb')
@@ -80,48 +80,4 @@ shutil.copyfile(SCRIPT_DIR + 'firmware/build/testing/bus_scan.uf2', args.out_dir
 shutil.copyfile(SCRIPT_DIR + 'firmware/build/testing/printer.uf2', args.out_dir + 'printer.uf2')
 
 quit()
-
-
-# OLD REMOVE ONCE FEATURE COMEPLETEN
-
-with open(sys.argv[1], "r") as f:
-    config = json.load(f)
-
-#config = json.load("config.json")
-os.makedirs(output_dir, exist_ok=True)
-
-print("Generating PCB from JSON...")
-generate(config, output_dir) # generate pcb file from json.
-print("Finished generating PCB.")
-
-pcb = os.path.join(output_dir, "output.kicad_pcb")
-
-print("labeling switch fingerprints...")
-label_switches(pcb) # add fingerprint labels to switches
-print("Finished labeling switched.")
-
-print("Finding Java install...")
-java = findJava()
-
-print("Calling routing script...")
-while(True):
-    try:
-        routing_attempts -= 1
-        routePCB(pcb, java) # sends pcb to autorouter
-    except subprocess.TimeoutExpired:
-        print(colored("Autorouter timed out.", "red"))
-        if routing_attempts > 0:
-            print(colored(f"Trying again. {routing_attempts} attempt(s) remaining.", "yellow"))
-            continue
-        else:
-            print(colored("[ERROR]: No attempts remaining. Adjust PCB and try again, or route board manually.\nReturning unrouted PCB.", "red"))
-    break
-print("Routing script done.")
-
-print("Generating case files...")
-build(pcb, output_dir) # builds the keyboard case
-print("Case files generated.")
-
-print()
-print("Pipeline script finished!")
 
